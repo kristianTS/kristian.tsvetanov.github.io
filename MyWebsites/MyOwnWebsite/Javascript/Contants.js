2788e75c1cf5efa7e36c8dd70da8a918db8dd027
kristianTS <98990533+kristianTS@users.noreply.github.com>
2024-08-01 00:31:30 +0300

    Delete MyWebsites directory

--- MyWebsites/MyOwnWebsite/Javascript/Contants.js
+++
@@ -1,6 +0,0 @@
-let toggleButton = document.getElementsByClassName('toggle-button')[0];
-let navbarLinks = document.getElementsByClassName('navigation')[0];
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-})
--- MyWebsites/MyOwnWebsite/Javascript/aboutUs.js
+++
@@ -1,6 +0,0 @@
-let toggleButton = document.getElementsByClassName('toggle-button')[0];
-let navbarLinks = document.getElementsByClassName('aboutUs-navMenu-Header')[0];
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-})
